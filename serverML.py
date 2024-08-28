ba13cb2021ec6833adc8f3d4370b6658af84477f
Gustavo Gutiérrez <132943080+GAGV03@users.noreply.github.com>
2024-08-27 18:52:49 -0600

    Avance de servidor

--- serverML.py
+++ serverML.py
@@ -1,9 +1,9 @@
 #Libraries
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template # type: ignore
 import numpy as np
-from load import joblib
+from load import joblib # type: ignore
 import os
-from werkzeug.utils import secure_filename
+from werkzeug.utils import secure_filename # type: ignore
 
 
 #Load model
@@ -13,4 +13,23 @@ dt = joblib.load('dt1.joblib')
 server = Flask(__name__)
 
 #Define a route to send a JSON data
-@server.route('/predictjason', methods=['POST'])
+@server.route('/predictjson', methods=['POST'])
+
+def predictjson():
+    #Procesar datos de entrada
+    data = request.json
+    print(data)
+    inputData = np.array([
+        data['pH'],
+        data['sulphates'],
+        data['alcohol']
+    ])
+    
+    #Predecir utilizando la entrada y el modelo
+    result = dt.predict(inputData.reshape(1,-1))
+    #Enviar respuesta
+    return jsonify({'prediction': str(result[0])})
+
+
+if __name__ == '__main__':
+    server.run(debug=False, host='0.0.0.0', port=8080)
